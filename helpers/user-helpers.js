b8a337e58e401de2daf6ea0d856ecb7987f0aabb
Nishal Dinesh <nishaldinesh3516j@gmail.com>
2022-07-08 11:51:42 +0530

    signin page created

---
+++ helpers/user-helpers.js
@@ -0,0 +1,8 @@
+var db = require('../config/connection');
+var collection = require('../config/collection');
+var bcrypt = require('bcrypt')
+var objectId=require('mongodb').ObjectId
+
+module.exports ={
+    
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var userHelpers = require('../helpers/user-helpers')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('user/home-page');
 });
-
+router.get('/user-signin',(req,res, next)=>{
+  res.render('user/signin')
+})
 module.exports = router;
