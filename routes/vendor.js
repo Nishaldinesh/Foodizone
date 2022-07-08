483dbf5a24499140c6ab338d3b5a551e77e9b43d
Nishal Dinesh <nishaldinesh3516j@gmail.com>
2022-07-08 23:15:26 +0530

    vendor signin and signup page added

--- routes/vendor.js
+++ routes/vendor.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/',(req,res, next)=>{
-    res.render('vendor/vendor-dashboard')
+    res.render('vendor/signup')
 });
+router.get('/signin',(req,res, next)=>{
+    res.render('vendor/signin')
+})
 
 module.exports =router;
